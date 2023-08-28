c3b44a460bd5b4519224da23093a5f318c70a925
Wesley Assis <88063740+WesleyAssiss@users.noreply.github.com>
2023-08-28 13:17:40 -0300

    Servidor.py

--- Sockets/Servidor.py
+++ Sockets/Servidor.py
@@ -2,25 +2,36 @@ import socket
 import json
 
 def main():
-    server_address = "127.0.0.1"
-    server_port = 8080  # Use port 8080
+    # Configurações do servidor
+    server_address = "127.0.0.1"  # Endereço do servidor (localhost)
+    server_port = 8080  # Número da porta que o servidor está ouvindo
 
     try:
+        # Cria um socket TCP/IP
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+            # Associa o socket ao endereço e porta especificados
             server_socket.bind((server_address, server_port))
+            # Coloca o socket em modo de escuta
             server_socket.listen()
 
             print("Servidor esperando por conexões...")
 
+            # Aceita uma conexão de um cliente e retorna um novo socket e o endereço do cliente
             client_socket, client_address = server_socket.accept()
             print("Cliente conectado:", client_address)
 
+            # Cria um stream de leitura associado ao socket do cliente
             with client_socket, client_socket.makefile('r') as in_stream:
+                # Loop para processar 5 mensagens
                 for i in range(5):
+                    # Lê uma linha da mensagem enviada pelo cliente e remove espaços em branco
                     user_input = in_stream.readline().strip()
 
-                    json_object = json.loads(user_input)
+                    # Desserializa a mensagem JSON em um objeto Python
+                    json_object = json.loads(user_input)# transformar essa sequência de bytes em um objeto Python
+                                                        # que o código possa manipular e acessar de maneira conveniente.
 
+                    # Extrai os campos da mensagem JSON
                     nome = json_object["Nome"]
                     cpf = json_object["CPF"]
                     idade = json_object["idade"]
@@ -28,10 +39,11 @@ def main():
 
                     print()
 
+                    # Formata e imprime a mensagem em diferentes formatos com base no valor de i
                     if i == 0:
                         print("Formato CSV:")
-                        formatted_message_csv = f"{{\"Nome\":\"{nome}\", \"CPF\":\"{cpf}\", \"idade\":{idade}, \"mensagem\":\"{mensagem}\"}}"
-                        print(formatted_message_csv + "\n")
+                        csv_message = f"Nome,CPF,idade,mensagem\n{nome},{cpf},{idade},{mensagem}"
+                        print(csv_message + "\n")
                     elif i == 1:
                         print("Formato JSON:")
                         ordered_json_string = (
@@ -69,7 +81,9 @@ def main():
                         print(toml_message + "\n")
 
     except Exception as e:
+        # Captura e imprime qualquer exceção que ocorra durante a execução do código
         print(e)
 
 if __name__ == "__main__":
+    # Chama a função main() se o script estiver sendo executado como o programa principal
     main()
